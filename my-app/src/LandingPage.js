30c382250eb6a5c2b45bac41568a02d8bbb5fa32
Deepak Mahapatra <deepakmahapatra047@gmail.com>
2023-03-22 16:41:48 +0530

    new

--- my-app/src/LandingPage.js
+++ my-app/src/LandingPage.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import "./styles/LandingPage.css"
 const LandingPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://panorbit.in/api/users.json')
+      .then(response => response.json())
+      .then(item => setData(item.users))
+  }, []);
+ 
   return (
     <div className='landing-container'>
-      
-      <h1>Hellow</h1>
+      <div className='user-account-cont'>
+        <div className='select-user'>Select an account</div>
+      <div className="user-accounts-container">
+      {data.map( user => (
+        <div key={user.id} className="user-card">
+          <img src={user.profilepicture} alt="User profile" />
+          <h3>{user.name}</h3>
+        </div>
+      ))}
+    </div>
+    </div>
     </div>
   )
 }
